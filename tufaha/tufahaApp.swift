c05bf4d1eaa9b312ce0a22e24d27934eda0f3a0e
MamboBryan <mambobryan@gmail.com>
2022-12-21 15:22:35 +0300

    :rocket:

---
+++ tufaha/tufahaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tufahaApp.swift
+//  tufaha
+//
+//  Created by MamboBryan on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct tufahaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
